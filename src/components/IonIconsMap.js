debf6853b168ec80072d5800df880288ba2f7a06
Viviane Brenner <brenner.vivi@gmail.com>
2022-03-18 10:44:51 -0300

    Preparando revisao

--- src/components/IonIconsMap.js
+++
@@ -1,9 +0,0 @@
-export default function IonIconsMap({icons}){
-    return (
-        <>
-            {icons.map((i) =>
-                <ion-icon name={i} key={i}></ion-icon>
-            )}
-        </>
-    );
-}
